e38dd848f04a126ac44876425a9b566c9fb9b1e2
jbroadway <johnny@johnnybroadway.com>
2012-03-06 16:49:22 -0600

    Added quick install routine to bootstrap.php, remove web installer, tests and .gitignore

--- bootstrap.php
+++ bootstrap.php
@@ -8,4 +8,44 @@ conf ('Database', 'master', array (
 	'pass' => $_SERVER["DB1_PASS"]
 ));
 
+// After the first run through, you can comment out or remove
+// this entire block and redeploy.
+if (! db_shift ('select count(*) from users')) {
+	// Run the db install
+	$sqldata = sql_split (file_get_contents ('conf/install_mysql.sql'));
+	db_execute ('begin');
+	foreach ($sqldata as $sql) {
+		if (! db_execute ($sql)) {
+			$err = db_error ();
+			db_execute ('rollback');
+			die ($err);
+		}
+	}
+
+	// Create a default admin user
+	$date = gmdate ('Y-m-d H:i:s');
+	if (! db_execute (
+		'insert into `user` (id, email, password, session_id, expires, name, type, signed_up, updated, userdata) values (1, ?, ?, null, ?, ?, "admin", ?, ?, ?)',
+		'you@example.com',
+		User::encrypt_pass ('ChangeMe'),
+		$date,
+		'Admin User',
+		$date,
+		$date,
+		json_encode (array ())
+	)) {
+		$err = db_error ();
+		db_execute ('rollback');
+		die ($err);
+	}
+
+	// The initial version history of the default page/blocks
+	$wp = new Webpage ('index');
+	Versions::add ($wp);
+	$b = new Block ('members');
+	Versions::add ($b);
+
+	db_execute ('commit');
+}
+
 ?>
